a6824ea710a07fb7afa2b7c4ce304d95007161e9
Taku Nakai <t.nakai.office@gmail.com>
2019-11-08 21:00:36 +0900

    first commit

---
+++ ray_trace.py
@@ -0,0 +1,39 @@
+import numpy as np
+import matplotlib.pyplot as plt
+import json
+import glob
+import sys
+import time
+import os
+import scipy.constants as cnt
+from unwrap.unwrap import unwrap
+from mpl_toolkits.axes_grid1 import make_axes_locatable
+from linecache import getline, clearcache
+from scipy.integrate import simps
+from optparse import OptionParser
+
+from OCC.Display.SimpleGui import init_display
+from OCC.gp import gp_Pln, gp_Trsf, gp_Lin, gp_Elips, gp_Elips2d
+from OCC.gp import gp_Pnt, gp_Vec, gp_Dir, gp_Ax1, gp_Ax2, gp_Ax3
+from OCC.BRep import BRep_Tool
+from OCC.TopLoc import TopLoc_Location
+from OCC.GeomLProp import GeomLProp_SurfaceTool
+from OCC.GeomAbs import GeomAbs_C2, GeomAbs_C0, GeomAbs_G1, GeomAbs_G2
+from OCC.GeomAPI import GeomAPI_ProjectPointOnSurf, GeomAPI_ProjectPointOnCurve
+from OCC.GeomAPI import GeomAPI_PointsToBSplineSurface, GeomAPI_IntCS
+from OCC.TColgp import TColgp_Array1OfPnt, TColgp_Array2OfPnt
+from OCC.BRepBuilderAPI import BRepBuilderAPI_MakeFace
+from OCC.Geom import Geom_Curve, Geom_Line, Geom_Ellipse
+from OCC.Geom import Geom_Plane, Geom_Surface, Geom_BSplineSurface
+from OCCUtils.Topology import Topo
+from OCCUtils.Construct import project_edge_onto_plane, project_point_on_curve
+from OCCUtils.Construct import make_wire, make_edge, make_plane, make_line, make_loft
+from OCCUtils.Construct import dir_to_vec, vec_to_dir
+
+if __name__ == "__main__":
+    argvs = sys.argv
+    parser = OptionParser()
+    parser.add_option("--dir", dest="dir", default="./")
+    opt, argc = parser.parse_args(argvs)
+    print(argc, opt)
+    
