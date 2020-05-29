f62fa775f063a250a481c4a0710a7a99a48a1c31
Taku Nakai <t.nakai.office@gmail.com>
2020-05-29 20:12:39 +0900

    update base

--- OCCDisplay.py
+++ OCCDisplay.py
@@ -7,11 +7,11 @@ import os
 from OCCQt import Viewer
 
 from OCC.Display.SimpleGui import init_display
-from OCC.Graphic3d import (Graphic3d_EF_PDF,
-                           Graphic3d_EF_SVG,
-                           Graphic3d_EF_TEX,
-                           Graphic3d_EF_PostScript,
-                           Graphic3d_EF_EnhPostScript)
+#from OCC.Graphic3d import (Graphic3d_EF_PDF,
+#                           Graphic3d_EF_SVG,
+#                           Graphic3d_EF_TEX,
+#                           Graphic3d_EF_PostScript,
+#                           Graphic3d_EF_EnhPostScript)
 
 from PyQt5.QtWidgets import QDialog, QLabel, QLineEdit, QPushButton, QVBoxLayout
 
@@ -39,13 +39,13 @@ class OCCDisplay (Viewer):
         indx = name.split(".")[-1]
 
         self.display.View.Dump(name)
-        if indx == "pdf":
-            ef_type = Graphic3d_EF_PDF
-        elif indx == "ps":
-            ef_type = Graphic3d_EF_PostScript
-        elif indx == "svg":
-            ef_type = Graphic3d_EF_SVG
-        else:
-            ef_type = Graphic3d_EF_PDF
+        #if indx == "pdf":
+        #    ef_type = Graphic3d_EF_PDF
+        #elif indx == "ps":
+        #    ef_type = Graphic3d_EF_PostScript
+        #elif indx == "svg":
+        #    ef_type = Graphic3d_EF_SVG
+        #else:
+        #    ef_type = Graphic3d_EF_PDF
         print(name)
         #self.display_obj.Export (name, ef_type)
--- base.py
+++ base.py
@@ -3,32 +3,175 @@ import matplotlib.pyplot as plt
 import matplotlib.tri as tri
 import sys
 import pickle
+import json
+import time
+import os
+import glob
+import shutil
+import datetime
+import platform
+from scipy.spatial import ConvexHull
+from optparse import OptionParser
 from matplotlib import animation
 from mpl_toolkits.axes_grid1 import make_axes_locatable
 from mpl_toolkits.mplot3d import Axes3D
+
+import logging
+logging.getLogger('matplotlib').setLevel(logging.ERROR)
+logging.getLogger('parso').setLevel(logging.ERROR)
 
 from OCC.Display.SimpleGui import init_display
 from OCC.Core.gp import gp_Pnt, gp_Vec, gp_Dir
 from OCC.Core.gp import gp_Ax1, gp_Ax2, gp_Ax3
 from OCC.Core.gp import gp_XYZ
-from OCC.Core.gp import gp_Lin
+from OCC.Core.gp import gp_Lin, gp_Elips
 from OCC.Core.gp import gp_Mat, gp_GTrsf, gp_Trsf
-from OCC.Core.TopoDS import TopoDS_Shape
+from OCC.Core.TopoDS import TopoDS_Shape, TopoDS_Compound
 from OCC.Core.TopLoc import TopLoc_Location
+from OCC.Core.TColgp import TColgp_Array1OfPnt, TColgp_Array2OfPnt
+from OCC.Core.TColgp import TColgp_HArray1OfPnt, TColgp_HArray2OfPnt
+from OCC.Core.BRep import BRep_Builder
+from OCC.Core.BRepFill import BRepFill_Filling
+from OCC.Core.BRepFill import BRepFill_CurveConstraint
+from OCC.Core.BRepOffset import BRepOffset_MakeOffset, BRepOffset_Skin, BRepOffset_Interval
 from OCC.Core.BRepPrimAPI import BRepPrimAPI_MakeSphere
 from OCC.Core.BRepBuilderAPI import BRepBuilderAPI_MakeWire
+from OCC.Core.BRepBuilderAPI import BRepBuilderAPI_MakeFace
 from OCC.Core.BRepBuilderAPI import BRepBuilderAPI_GTransform
-from OCCUtils.Construct import make_box, make_line, make_wire
+from OCC.Core.GeomAPI import geomapi
+from OCC.Core.GeomAPI import GeomAPI_PointsToBSplineSurface
+from OCC.Core.GeomAPI import GeomAPI_PointsToBSpline
+from OCC.Core.GeomAPI import GeomAPI_Interpolate
+from OCC.Core.GeomAbs import GeomAbs_C0, GeomAbs_C1, GeomAbs_C2
+from OCC.Core.GeomAbs import GeomAbs_G1, GeomAbs_G2
+from OCC.Core.GeomAbs import GeomAbs_Intersection, GeomAbs_Arc
+from OCC.Core.GeomFill import GeomFill_BoundWithSurf
+from OCC.Core.GeomFill import GeomFill_BSplineCurves
+from OCC.Core.GeomFill import GeomFill_StretchStyle, GeomFill_CoonsStyle, GeomFill_CurvedStyle
+from OCC.Core.AIS import AIS_Manipulator
+from OCC.Extend.DataExchange import write_step_file, read_step_file
+from OCCUtils.Topology import Topo
+from OCCUtils.Construct import make_box, make_line, make_wire, make_edge
 from OCCUtils.Construct import make_plane, make_polygon
 from OCCUtils.Construct import point_to_vector, vector_to_point
 from OCCUtils.Construct import dir_to_vec, vec_to_dir
 
-import logging
-logging.getLogger('matplotlib').setLevel(logging.ERROR)
-
 from Surface import surf_curv
 from OCCQt import Viewer
 
+def split_filename(filename="./temp_20200408000/not_ignore.txt"):
+    name = os.path.basename(filename)
+    rootname, ext_name = os.path.splitext(name)
+    return name, rootname
+
+
+def create_tempdir(name="temp", flag=1):
+    print(datetime.date.today())
+    datenm = "{0:%Y%m%d}".format(datetime.date.today())
+    dirnum = len(glob.glob("./{}_{}*/".format(name, datenm)))
+    if flag == -1 or dirnum == 0:
+        tmpdir = "./{}_{}{:03}/".format(name, datenm, dirnum)
+        os.makedirs(tmpdir)
+        fp = open(tmpdir + "not_ignore.txt", "w")
+        fp.close()
+    else:
+        tmpdir = "./{}_{}{:03}/".format(name, datenm, dirnum - 1)
+    return tmpdir
+
+
+def create_tempnum(name, tmpdir="./", ext=".tar.gz"):
+    num = len(glob.glob(tmpdir + name + "*" + ext)) + 1
+    filename = '{}{}_{:03}{}'.format(tmpdir, name, num, ext)
+    #print(num, filename)
+    return filename
+
+
+class SetDir (object):
+
+    def __init__(self):
+        self.root_dir = os.getcwd()
+        self.tempname = ""
+        self.rootname = ""
+        self.create_tempdir()
+
+        pyfile = sys.argv[0]
+        self.filename = os.path.basename(pyfile)
+        self.rootname, ext_name = os.path.splitext(self.filename)
+        self.tempname = self.tmpdir + self.rootname
+        print(self.rootname)
+
+    def init(self):
+        self.tempname = self.tmpdir + self.rootname
+
+    def create_tempdir(self, name="temp", flag=1):
+        self.tmpdir = create_tempdir(name, flag)
+        self.tempname = self.tmpdir + self.rootname
+        print(self.tmpdir)
+
+    def add_dir(self, name="temp"):
+        dirnum = len(glob.glob("{}/{}/".format(self.tmpdir, name)))
+        if dirnum == 0:
+            tmpdir = "{}/{}/".format(self.tmpdir, name)
+            os.makedirs(tmpdir)
+            fp = open(tmpdir + "not_ignore.txt", "w")
+            fp.close()
+            print("make {}".format(tmpdir))
+        else:
+            tmpdir = "{}/{}/".format(self.tmpdir, name)
+            print("already exist {}".format(tmpdir))
+        return tmpdir
+
+def pnt_from_axs(axs=gp_Ax3(), length=100):
+    vec = point_to_vector(axs.Location()) + \
+        dir_to_vec(axs.Direction()) * length
+    return vector_to_point(vec)
+
+
+def line_from_axs(axs=gp_Ax3(), length=100):
+    return make_edge(axs.Location(), pnt_from_axs(axs, length))
+
+
+def spl_face(px, py, pz, axs=gp_Ax3()):
+    nx, ny = px.shape
+    pnt_2d = TColgp_Array2OfPnt(1, nx, 1, ny)
+    for row in range(pnt_2d.LowerRow(), pnt_2d.UpperRow() + 1):
+        for col in range(pnt_2d.LowerCol(), pnt_2d.UpperCol() + 1):
+            i, j = row - 1, col - 1
+            pnt = gp_Pnt(px[i, j], py[i, j], pz[i, j])
+            pnt_2d.SetValue(row, col, pnt)
+            #print (i, j, px[i, j], py[i, j], pz[i, j])
+
+    api = GeomAPI_PointsToBSplineSurface(pnt_2d, 3, 8, GeomAbs_G2, 0.001)
+    api.Interpolate(pnt_2d)
+    #surface = BRepBuilderAPI_MakeFace(curve, 1e-6)
+    # return surface.Face()
+    face = BRepBuilderAPI_MakeFace(api.Surface(), 1e-6).Face()
+    face.Location(set_loc(gp_Ax3(), axs))
+    return face
+
+
+def spl_curv(px, py, pz):
+    num = px.size
+    pts = []
+    p_array = TColgp_Array1OfPnt(1, num)
+    for idx, t in enumerate(px):
+        x = px[idx]
+        y = py[idx]
+        z = pz[idx]
+        pnt = gp_Pnt(x, y, z)
+        pts.append(pnt)
+        p_array.SetValue(idx + 1, pnt)
+    api = GeomAPI_PointsToBSpline(p_array)
+    return p_array, api.Curve()
+
+
+def spl_curv_pts(pts=[gp_Pnt()]):
+    num = len(pts)
+    p_array = TColgp_Array1OfPnt(1, num)
+    for idx, pnt in enumerate(pts):
+        p_array.SetValue(idx + 1, pnt)
+    api = GeomAPI_PointsToBSpline(p_array)
+    return p_array, api.Curve()
 
 def pnt_trf_vec(pnt=gp_Pnt(), vec=gp_Vec()):
     v = point_to_vector(pnt)
@@ -78,28 +221,53 @@ def rot_axs(axis=gp_Ax3(), pxyz=[0, 0, 0], rxyz=[0, 0, 0]):
     trsf = set_trf(gp_Ax3(), axs)
     axis.Transform(trsf)
 
-class plotocc (Viewer):
+class plotocc (Viewer, SetDir):
 
     def __init__(self):
         self.display, self.start_display, self.add_menu, self.add_functionto_menu = init_display()
+        SetDir.__init__(self)
         Viewer.__init__(self)
         self.on_select()
+        self.SaveMenu()
 
-    def show_box(self):
-        self.display.DisplayShape(gp_Pnt())
-        self.display.DisplayShape(make_box(100, 100, 100))
+    def show_box(self, axs=gp_Ax3(), lxyz=[100, 100, 100]):
+        box = make_box(*lxyz)
+        ax1 = gp_Ax3(
+            gp_Pnt(-lxyz[0] / 2, -lxyz[1] / 2, -lxyz[2] / 2),
+            gp_Dir(0, 0, 1)
+        )
+        trf = gp_Trsf()
+        trf.SetTransformation(axs, gp_Ax3())
+        trf.SetTransformation(ax1, gp_Ax3())
+        box.Location(TopLoc_Location(trf))
+        self.display.DisplayShape(axs.Location())
+        self.show_axs_pln(axs, scale=lxyz[0])
+        self.display.DisplayShape(box, transparency=0.7)
 
     def show_pnt(self, xyz=[0, 0, 0]):
         self.display.DisplayShape(gp_Pnt(*xyz))
+
+    def show_pts(self, pts=[gp_Pnt()], num=1):
+        for p in pts[::num]:
+            self.display.DisplayShape(p)
+        self.display.DisplayShape(make_polygon(pts))
 
     def show_ball(self, scale=100, trans=0.5):
         shape = BRepPrimAPI_MakeSphere(scale).Shape()
         self.display.DisplayShape(shape, transparency=trans)
 
+    def show_vec(self, beam=gp_Ax3(), scale=1.0):
+        pnt = beam.Location()
+        vec = dir_to_vec(beam.Direction()).Scaled(scale)
+        print(vec.Magnitude())
+        self.display.DisplayVector(vec, pnt)
+
     def show_ellipsoid(self, axs=gp_Ax3(), rxyz=[10., 10., 10.], trans=0.5):
         shape = gen_ellipsoid(axs, rxyz)
         self.display.DisplayShape(shape, transparency=trans, color="BLUE")
         return shape
+    
+    
 
     def show_axs_pln(self, axs=gp_Ax3(), scale=100):
         pnt = axs.Location()
@@ -117,6 +285,30 @@ class plotocc (Viewer):
         self.display.DisplayShape(make_line(pnt, pnt_x), color="RED")
         self.display.DisplayShape(make_line(pnt, pnt_y), color="GREEN")
         self.display.DisplayShape(make_line(pnt, pnt_z), color="BLUE")
+    
+    def show_plane(self, axs=gp_Ax3(), scale=100):
+        pnt = axs.Location()
+        vec = dir_to_vec(axs.Direction())
+        pln = make_plane(pnt, vec, -scale, scale, -scale, scale)
+        self.display.DisplayShape(pln)
+
+    def make_EllipWire(self, rxy=[1.0, 1.0], shft=0.0, axs=gp_Ax3()):
+        rx, ry = rxy
+        if rx > ry:
+            major_radi = rx
+            minor_radi = ry
+            axis = gp_Ax2()
+            axis.SetXDirection(axis.XDirection())
+        else:
+            major_radi = ry
+            minor_radi = rx
+            axis = gp_Ax2()
+            axis.SetXDirection(axis.YDirection())
+        axis.Rotate(axis.Axis(), np.deg2rad(shft))
+        elip = make_edge(gp_Elips(axis, major_radi, minor_radi))
+        poly = make_wire(elip)
+        poly.Location(set_loc(gp_Ax3(), axs))
+        return poly
 
     def show_plane(self, axs=gp_Ax3(), scale=100):
         pnt = axs.Location()
@@ -124,8 +316,21 @@ class plotocc (Viewer):
         pln = make_plane(pnt, vec, -scale, scale, -scale, scale)
         self.display.DisplayShape(pln)
 
+    def SaveMenu(self):
+        self.add_menu("File")
+        self.add_function("File", self.export_cap)
+
+    def export_cap(self):
+        pngname = create_tempnum(self.rootname, self.tmpdir, ".png")
+        self.display.View.Dump(pngname)
+
+    def export_stp(self, shp):
+        stpname = create_tempnum(self.rootname, self.tmpdir, ".stp")
+        write_step_file(shp, stpname)
+
     def show(self):
         self.display.FitAll()
+        self.display.View.Dump(self.tempname + ".png")
         self.start_display()
 
 
